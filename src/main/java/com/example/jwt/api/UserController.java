61d20efb93291e0df520cdb807f4661c6605d81e
anhtan7d123 <tanltn123456@gmail.com>
2023-02-02 21:35:16 +0700

    update pom

--- src/main/java/com/example/jwt/api/UserController.java
+++ src/main/java/com/example/jwt/api/UserController.java
@@ -1,4 +1,24 @@
 package com.example.jwt.api;
 
+import com.example.jwt.domain.User;
+import com.example.jwt.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/get-all-user")
+    public ResponseEntity<List<User>> getAllUser(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 }
